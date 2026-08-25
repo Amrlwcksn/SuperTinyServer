dfc8f75a185e880ae974fb4c1bb3f5eb0e644007
amrlwcksn <amrlwcksn@gmail.com>
2026-08-25 07:28:58 +0700

    update UI

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 /*
